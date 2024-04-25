b184ced697eefd0c040fd04ca2ceda41c8416fa8
vib3sf <vib3sf@proton.me>
2024-04-25 21:55:02 +0300

    make row collapsing

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,15 +1,49 @@
+import { useEffect, useState } from "react";
 import { biomApi } from "../../services/bioms/bioms.api";
 import { BiomDto } from "../../services/bioms/bioms.dto";
 import "./Row.css";
 
 export function Row({ biomElem }: { biomElem: BiomDto }) {
+  const [isActive, setIsActive] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const collapseRow = windowWidth < 640;
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
-    <tr>
-      {Object.values(biomElem).map((elem, index) => (
-        <td key={index} data-label={biomApi.getTableHead()[index]}>
-          {elem}
-        </td>
-      ))}
-    </tr>
+    <>
+      {collapseRow ? (
+        <button
+          type="button"
+          className="collapsible"
+          onClick={() => setIsActive(!isActive)}
+        >
+          {biomElem.name}
+        </button>
+      ) : (
+        ""
+      )}
+      {isActive || !collapseRow ? (
+        <tr>
+          {Object.values(biomElem).map((elem, index) => (
+            <td key={index} data-label={biomApi.getTableHead()[index]}>
+              {elem}
+            </td>
+          ))}
+        </tr>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
